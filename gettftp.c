29e1de313ff53766d75650927b15263137e23b6b
badr07X <badrettousy26@gmail.com>
2024-12-17 09:15:34 +0100

    Étape 1 : Lecture des arguments en ligne de commande

--- gettftp.c
+++ gettftp.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s host file\n", argv[0]);
+    if (argc != 3) {  // Vérifie si 2 arguments sont passés
+        printf("Usage: %s <host> <file>\n", argv[0]);
         return 1;
     }
+
     char *host = argv[1];
     char *file = argv[2];
+
     printf("Host: %s, File: %s\n", host, file);
     return 0;
 }
